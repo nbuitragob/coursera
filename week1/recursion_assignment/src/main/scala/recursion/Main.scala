e2eee9c74eb435a0a5d28668222dc395d63faaea
nbuitragob <nbuitragob@github.com>
2018-06-13 16:31:01 -0500

    week1 finished, count change doen't work though

--- week1/recursion_assignment/src/main/scala/recursion/Main.scala
+++ week1/recursion_assignment/src/main/scala/recursion/Main.scala
@@ -3,7 +3,7 @@ package recursion
 object Main {
   def main(args: Array[String])={}
 
-  def pascal(c: Int, r: Int) ={
+  def pascal(c: Int, r: Int): Int = {
     def factorial(n: Int)={
       def factorialIterator(acc: Int, n: Int): Int ={
         if(n == 0) acc else factorialIterator(acc * n, n - 1)
@@ -17,34 +17,77 @@ object Main {
 
   }
 
-  def checkParensBalancing(phrase: String): Any = {
+  //This algorithm fails 
+  
+  def countChange(money: Int, coins: List[Int]): Int = {
+    val sortedCoins = coins.toIndexedSeq.sorted
+    val size = sortedCoins.length
+    def countChange(currentMoney: Int, count: Int, head: Int, i: Int): Int = {
+      if(head + 1 >= size) count
+      else if(i == 0) countChange(money % (head + 1), count, head + 1, (money - money % sortedCoins(head + 1))/sortedCoins(head + 1))
+      else
+          if(currentMoney == 0) 
+            countChange(money - sortedCoins(head)*(i-1), count + 1, head, i-1)
+
+          else if(currentMoney%sortedCoins(head + 1) == currentMoney)    
+            countChange(money - sortedCoins(head)*(i-1), count, head, i-1)
+
+          else if(currentMoney%sortedCoins(head + 1) == 0)
+            countChange(money - sortedCoins(head)*(i-1), count + 1, head, i-1)
+          
+
+          else countChange(currentMoney%(head + 1), count, head, i)
+         
+    }
+
+    if(money == 0 || size == 0) 0
+    else countChange(money % sortedCoins(0), 0, 0, (money - money % sortedCoins(0))/sortedCoins(0))
+  } 
+
+
+  /*
+
+  Solution:
+
+  def countChange(amount: Int): Int = {
+    def firstDenomination(kindsOfCoins: Int): Int = {
+      kindsOfCoins match {
+        case 1 => 1
+        case 2 => 5
+        case 3 => 10
+        case 4 => 25
+        case 5 => 50
+      }
+    }
+
+    def cc(amount: Int, kindOfCoins: Int): Int = {
+      if (amount == 0) 1
+      else if (amount < 0 || kindOfCoins == 0) 0
+      else cc(amount, kindOfCoins - 1) + cc(amount - firstDenomination(kindOfCoins), kindOfCoins)
+    }
+
+    cc(amount, 5)
+  }
+  countChange(100) //should be 292
+
+  */
+
+
+  def balance(chars: List[Char]): Boolean = {
     def isBalanced(phrase: List[Char], isClosing: Boolean, nOpeningParens: Int): Boolean = {
-      phrase match{
-        case Nil => isClosing && nOpeningParens == 0
-        case x :: xs => {
-          if((x != '(' && nOpeningParens == 0) || nOpeningParens < 0) false
-          else if(x == '(') isBalanced(xs, false, nOpeningParens + 1)
-          else isBalanced(xs, true, nOpeningParens - 1)
+        phrase match{
+          case Nil => isClosing && nOpeningParens == 0
+          case x :: xs => {
+            if((x == ')' && nOpeningParens == 0) || nOpeningParens < 0) false
+            else if(x == '(') isBalanced(xs, false, nOpeningParens + 1)
+            else if(x == ')') isBalanced(xs, true, nOpeningParens - 1)
+            else isBalanced(xs, false, nOpeningParens)
         }
       }
     
     }
 
-    isBalanced(getParens(phrase.toList), false, 0)
-  }
-
-  def getParens(phrase: List[Char]) ={
-    def selectParens(current_phrase: List[Char], ans: List[Char]): List[Char] ={
-      if(current_phrase.isEmpty) ans 
-      else if(isParens(current_phrase.head)) selectParens(current_phrase.tail, ans ::: List(current_phrase.head))
-      else selectParens(current_phrase.tail, ans) 
-    }
-
-    def isParens(x: Char)=
-      x == '(' || x == ')'
-
-    selectParens(phrase, Nil)
-  
+    isBalanced(chars, false, 0)
   }
   
 }
--- week1/recursion_assignment/src/test/scala/recursion/MainSpec.scala
+++ week1/recursion_assignment/src/test/scala/recursion/MainSpec.scala
@@ -15,18 +15,31 @@ class MainSpec extends FlatSpec with Matchers {
     Main.pascal(1,3) shouldEqual 3
   }
 
-  ("Main.getParens((if (zero? x) max (/ 1 x)))" 
-        should "return (()())") in {
-    
-    (Main.getParens("(if (zero? x) max (/ 1 x))".toList) 
-        shouldEqual List('(', '(', ')', '(', ')', ')'))
-  } 
-
-  ("Main.checkParensBalancing('(if (zero? x) max (/ 1 x))')" 
+  ("Main.balance('(if (zero? x) max (/ 1 x))')" 
         should "return true") in {
     
-    (Main.checkParensBalancing("(if (zero? x) max (/ 1 x))") 
+    (Main.balance("(if (zero? x) max (/ 1 x))".toList) 
         shouldEqual true)
+  }
+
+  "Main.countChange(4, List(1,2))" should "return 3" in {
+    Main.countChange(4, List(1,2)) shouldEqual 3
+  }
+
+  "Main.countChange(4, List(1,2,3))" should "return 3" in {
+    Main.countChange(4, List(1,2,3)) shouldEqual 4
+  }
+
+  "Main.countChange(222, List(1,5,10,20,50,100)" should "return 3" in {
+    Main.countChange(222, List(1,5,10,20,50,100)) shouldEqual 3
+  }
+
+  "Main.countChange(300,List(500,5,50,100,20,200,10))" should "return 1022" in {
+    Main.countChange(300,List(500,5,50,100,20,200,10)) shouldEqual 1022 
+  }
+
+  "Main.countChange(100,List(1,10,5,10,25,50))" should "return 292" in {
+    Main.countChange(100,List(1,10,5,10,25,50)) shouldEqual 292 
   }
 
 }
